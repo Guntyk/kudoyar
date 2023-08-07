b931da78183b03c4332f4a03a3c388ef298a5d78
Gunter <mashyn.daniil@gmail.com>
2023-08-07 11:44:02 +0300

    Header Done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,39 @@
-import Button from "../../common/Button/Button";
+import logo from "../../materials/logo.png";
+import "./Header.css";
 
 export default function Header() {
-  return <Button buttonText="Замовити консультацію" />;
+  return (
+    <div className="container">
+      <header className="header">
+        <a href="/" className="logo-link">
+          <img src={logo} alt="Логотип 'Кудояр'" className="logo-img" />
+          <span className="logo">Кудояр</span>
+        </a>
+        <nav>
+          <ul className="text links">
+            <li>
+              <a href="#services" className="linked">
+                Послуги
+              </a>
+            </li>
+            <li>
+              <a href="#articles" className="linked">
+                Статті
+              </a>
+            </li>
+            <li>
+              <a href="#testimonials" className="linked">
+                Відгуки
+              </a>
+            </li>
+            <li>
+              <a href="#contacts" className="linked">
+                Контакти
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
